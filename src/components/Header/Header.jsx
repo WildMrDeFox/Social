e09e09fb564c5dd20d47423345a7339bfac4fc99
WildMrDeFox <wildmrdefox01@gmail.com>
2021-11-01 21:33:49 +0700

    Авторизация

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import h from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-function Header() {
+function Header(props) {
     return (
         <header className={h.header}>
             <img src="https://cdn.pixabay.com/photo/2018/03/27/15/05/logo-3266214_1280.png" alt=""/>
+
+            <div className={h.loginBlock}>
+                {props.isAuth ? props.login
+                    : <NavLink to={'/login'}>Login</NavLink> }
+            </div>
         </header>
     );
 }
